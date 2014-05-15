acb55af66e942a4fe12a78460da04fca1c619025
Lisovsky Vlad <lisovskyvlad@gmail.com>
2014-05-16 02:16:50 +0400

    add two tests

--- spec/cities_spec.rb
+++ spec/cities_spec.rb
@@ -7,7 +7,9 @@ describe '#cities_water_summ' do
     [5,1,5] => 4,
     [2,7,2,7,4,7,1,7,3,7] => 5+3+6+4,
     [5,1,0,1] => 1,
-    [2,5,1,2,3,4,7,7,6,3,5] => 12
+    [2,5,1,2,3,4,7,7,6,3,5] => 12,
+    [5,4,3,2,3,8] => 8,
+    [5,4,3,5,3,8] => 5
   }.each do |heights, volume|
     it 'should return volume of water' do
       expect(cities_water_summ(heights)).to eq(volume)
